575d4e69e15cbc691e53417ecd6fe758432ae1b5
andrew <grebenkovandrew@gmail.com>
2018-05-12 13:36:18 +0300

    common

---
+++ src/main/java/Application.java
@@ -0,0 +1,12 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Bucky on 12.05.2018.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/controller/MoneyTransferController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Bucky on 12.05.2018.
+ */
+
+@Controller
+public class MoneyTransferController {
+
+
+}
